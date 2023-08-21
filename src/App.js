38be62198413b7514570f0d7dffdb3d0b2b27da0
sohaib-khan-niazi <sohaibkhanniazi1997@gmail.com>
2023-08-21 19:17:25 +0500

    initial code

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-indigo-500 h-screen items-center'>
+      <p className='flex justify-center items-center text-[32px] font-bold text-gray-400'>React-app-ec2</p>
+      </div>
+  )
+}
+
+export default App
